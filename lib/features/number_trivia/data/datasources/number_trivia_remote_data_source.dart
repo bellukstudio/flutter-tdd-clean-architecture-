6952c8a0d34e2db98761db5322961d74e41c45fb
bellukchips <bellukchips@gmail.com>
2022-08-20 09:54:04 +0700

    remote data source & unit testing

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter_tdd_learn/core/error/exeptions.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter_tdd_learn/features/number_trivia/data/models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDataSource {
@@ -10,4 +15,30 @@ abstract class NumberTriviaRemoteDataSource {
   ///
   /// throws a [ServerException] for all error codes
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+  NumberTriviaRemoteDataSourceImpl({
+    required this.client,
+  });
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int? number) async {
+    return _getTriviaUrl('http://numbersapi.com/$number');
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() async {
+    return _getTriviaUrl('http://numbersapi.com/random');
+  }
+
+  Future<NumberTriviaModel> _getTriviaUrl(String url) async {
+    final response = await client
+        .get(Uri.parse(url), headers: {'Content-Type:': 'application/json'});
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
